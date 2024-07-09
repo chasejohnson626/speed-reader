a1b2478dbd5e2099b13b0a5b6dae6e9aeda47470
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-07-09 03:15:51 -0600

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ toggleDarkMode }) => {
       <div className='flex-grow basis-0'>
         <h1 className='float-left'>Speed Reading</h1>
       </div>
-      <h1>Created by <a href="https://cjwebdev.com/" className='underline text-info'>CJ Web Dev</a></h1>
+      <h1>Created by Chase Johnson</h1>
       <div className='flex-grow basis-0'>
         <button onClick={toggleDarkMode} className='float-right bg-primary text-primary-content shadow-md rounded-3xl hover:bg-primary-focus'>
           <BsMoonStarsFill className='m-3 w-6 h-6' />
